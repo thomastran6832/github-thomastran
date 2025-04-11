5ffda0fc87413d13a0b54018a05ae9a398f0346c
thomastran6832 <thomastran6832@gmail.com>
2025-04-11 19:42:47 +0700

    commit

--- data/user-data.js
+++ data/user-data.js
@@ -1,9 +1,9 @@
 export const userData = {
   githubUser: 'thomastran6832',
   devUsername: "thomastran6832",
-  name: 'Thomas Tran',
-  designation: 'Devops Engineer',
-  avatarUrl: 'https://avatars.githubusercontent.com/u/103546648?v=4',
+  // name: 'Thomas Tran',
+  // designation: 'Devops Engineer',
+  // avatarUrl: 'https://avatars.githubusercontent.com/u/103546648?v=4',
   github: 'https://github.com/thomastran6832',
   facebook: '',
   linkedIn: '',
